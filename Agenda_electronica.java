dcad30fb98650460c83ff7b85503d2377b7c8fcb
Cesar Briseño <142867500+CesarBr1397@users.noreply.github.com>
2024-06-03 00:06:52 -0600

    Add files via upload

---
+++ Agenda_electronica.java
@@ -0,0 +1,14 @@
+package agenda_electronica;
+import agenda_electronica.igu.Principal;
+
+public class Agenda_electronica {
+
+	public static void main(String[] args) {
+		
+		Principal princ = new Principal();
+		princ.setVisible(true);
+		princ.setLocationRelativeTo(null);
+		
+	}
+	
+}
